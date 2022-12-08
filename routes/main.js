7f3272c7ac1bdda596563805e96577ec1490f089
Isabela Ribeiro Baséggio <isabela.baseggio7@gmail.com>
2022-12-08 17:57:57 -0300

    main route, MainController, views main
    get /resister, MainController mainRegister, views main register.ejs

--- routes/main.js
+++ routes/main.js
@@ -3,4 +3,6 @@ const MainController = require("../controllers/MainController")
 
 router.get("/", MainController.mainIndex);
 
+router.get("/register", MainController.mainRegister);
+
 module.exports = router;
