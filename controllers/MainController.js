b0be05b06fa1b91cb2f3dce4befdeece337a897d
Isabela Ribeiro Baséggio <isabela.baseggio7@gmail.com>
2023-01-05 23:58:26 -0300

    mainController resetPage

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -6,7 +6,40 @@ const mainIndex = (req, res) => {
   }
 };
 
+const resetPage = (req, res) => {
+  try {
+    res.render("main/reset", {messages: null, values: null});
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
+const resetPassword = (req, res) => {
+  messages = [];
+  let values = {
+    email: req.body.email,
+  };
+
+  if (
+    !values.email ||
+    typeof values.email == undefined ||
+    values.email == null) {
+    messages.push({ text: "This email address is not registered" });
+  }
+
+  if (messages.length > 0) {
+    typeMsg = "danger";
+    res.render("main/reset", {
+      messages: messages,
+      type: typeMsg,
+      values: values,
+    });
+  }
+}
+
 
 module.exports = {
   mainIndex,
+  resetPage,
+  resetPassword,
 };
--- routes/main.js
+++ routes/main.js
@@ -3,4 +3,8 @@ const MainController = require("../controllers/MainController")
 
 routes.get("/", MainController.mainIndex);
 
+routes.get('/reset', MainController.resetPage);
+
+routes.post('/reset', MainController.resetPassword);
+
 module.exports = routes;
