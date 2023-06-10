757ebce3cb8024a8b92d04ed5dd76c0e45ee94e7
Isabela Ribeiro Baséggio <isabela.baseggio7@gmail.com>
2023-06-10 08:27:14 -0300

    conn db

--- database/db.js
+++ database/db.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 
-const connectDB = () => {
-  mongoose
+const connectDB = async () => {
+  try { 
+    const conn = await mongoose
     .connect(process.env.DB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    })
-    .then(() => console.log("MongoDB Atlas Connected"))
-    .catch((err) => console.log(err));
-};
+    });
+    console.log("MongoDB Atlas Connected");
+} catch(err) {
+  console.log(err);
+}
+}
 
 module.exports = connectDB;
