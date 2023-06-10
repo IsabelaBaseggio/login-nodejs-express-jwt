bcc06166d21a2ea41e72838a8e671a3e260aab39
Isabela Ribeiro Baséggio <isabela.baseggio7@gmail.com>
2023-06-10 08:16:28 -0300

    cyclic mongo

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const path = require("path");
 const mainRoute = require("./routes/main");
 const userRoute = require("./routes/user");
 const adminRoute = require("./routes/admin");
-const session = require('express-session');
-const flash = require('connect-flash');
+const session = require("express-session");
+const flash = require("connect-flash");
 const connectDB = require("./database/db");
 const passport = require("passport");
 require("./controllers/AuthUserController")(passport);
@@ -16,17 +16,17 @@ const app = express();
 // Port Number
 const port = process.env.PORT || 3000;
 
-
 //CONFIGS
 
 // Session, Cookies & Flash
-app.use(session({
-  cookie:{ maxAge: 2628000000},
-  secret: process.env.SECRET,
-  resave: true,
-  saveUninitialized: false
-}));
-
+app.use(
+  session({
+    cookie: { maxAge: 2628000000 },
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: false,
+  })
+);
 
 // Passport
 app.use(passport.initialize());
@@ -36,11 +36,11 @@ app.use(flash());
 
 // Middleware - Flash
 app.use((req, res, next) => {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
   res.locals.user = req.user || null;
   next();
-})
+});
 
 // Parse JSON Data
 app.use(express.urlencoded({ extended: true }));
@@ -48,9 +48,6 @@ app.use(express.json());
 
 // EJS
 app.set("view engine", "ejs");
-
-// Mongoose - DB Connection
-connectDB();
 
 // Public
 app.use(express.static(path.join(__dirname, "public")));
@@ -60,7 +57,10 @@ app.use("/", express.json(), mainRoute);
 app.use("/user", express.json(), userRoute);
 app.use("/admin", express.json(), adminRoute);
 
-// Server
-app.listen(port, "0.0.0.0", () => {
-  console.log(`Server running in ${port}`);
+// Mongoose - DB Connection
+connectDB().then(() => {
+  // Server
+  app.listen(port, "0.0.0.0", () => {
+    console.log(`Server running in ${port}`);
+  });
 });
